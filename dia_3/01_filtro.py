dacc2b1164221bdb2747ce55f088515b39e94624
Matheus Souza <matheuselia_@hotmail.com>
2025-03-17 23:26:58 -0300

    mais_filtros

--- dia_3/01_filtro.py
+++ dia_3/01_filtro.py
@@ -7,7 +7,7 @@ df.head()
 
 # %%
 
-pontos = [10, 1, 1, 1, 50, 100, 130, 30, 25, 50]
+pontos = [10, True, True, True, 50, 100, 130, 30, 25, 50]
 filtro = []
 
 valores_50_mais = []
@@ -20,3 +20,35 @@ filtro
 # %%
 
 resultado = []
+for i in range(len(pontos)):
+    resultado.append(pontos[i] >= 50)
+
+resultado
+# %%
+
+info = pd.DataFrame({"nome": ["matheus", "ana", "joão"],
+                          "idade": [26, 25, 30],
+                          "uf": ["RJ", "SP", "MG"]})
+
+filtro = info["idade"] > 26
+info[filtro]
+
+# %%
+
+df = pd.read_csv("../data/transacoes.csv")
+df.head()
+
+# %%
+
+filtro = df["qtdePontos"] >= 50
+df[filtro]
+
+# %%
+
+filtro = (df["qtdePontos"] >= 50) & (df["qtdePontos"] >= 100)
+df[filtro]
+
+# %%
+
+filtro = (df["qtdePontos"] == True) | (df["qtdePontos"] == 100)
+df[filtro]
